9b3e12fd9117b76f10684a9321c7257cf43a5042
Manol Manolov <manolow9621@gmail.com>
2023-08-05 13:35:38 +0300

    Solved a problem that prints numbers names from 1 to 999

---
+++ src/book1/SumOfOddNumsFom1ToNWithout7.java
@@ -0,0 +1,21 @@
+package book1;
+
+import java.util.Scanner;
+
+public class SumOfOddNumsFom1ToNWithout7 {
+    public static void main(String[] args) {
+
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = Input.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i += 2){
+            if (i % 7 == 0){
+                continue;
+            }
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
